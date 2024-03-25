784aea97b46e61b664dcc21a29c0375775653a48
Caro <carolinegraebel@live.de>
2024-03-25 16:24:49 +0100

    Update Functional Programming
    Ascii-freundliche Funktionen und Tests

--- Functional Programming/Functional_Programming.py
+++ Functional Programming/Functional_Programming.py
@@ -1,18 +1,18 @@
 """
 Funktionen ohne Seiteneffekte: alle
-Higher-Order functions: closure_Fibonacci()
+Higher-Order functions: closure_fibonacci()
 Funktionen als Parameter und return values:
-    closure_Fibonacci() - gibt Funktion berechne_Fib_Zahl() zurueck
-    anonyme_Differentiation() - Parameter f ist eine Funktion (siehe Test-Datei)
-Nutzung closure Funktion: closure_Fibonacci()
-Nutzung anonymer Funktion: anonyme_Differentiation()
+    closure_fibonacci() - gibt Funktion berechne_fib_zahl() zurueck
+    anonyme_differentiation() - Parameter f ist eine Funktion (siehe Test-Datei)
+Nutzung closure Funktion: closure_fibonacci()
+Nutzung anonymer Funktion: anonyme_differentiation()
 """
 
-def closure_Fibonacci():
+def closure_fibonacci():
     """Diese Funktion generiert mit jedem wiederholten Call die naechste Fibonacci-Zahl."""
     num1 = 0
     num2 = 0
-    def berechne_Fib_Zahl():
+    def berechne_fib_zahl():
         """Diese Helferfunktion berechnet Fibonacci-Zahlen."""
         nonlocal num1, num2
         if num1 ==0 and num2 ==0:
@@ -23,19 +23,19 @@ def closure_Fibonacci():
             num2 = num1
             num1 = n
             return n
-    return berechne_Fib_Zahl
+    return berechne_fib_zahl
 
-def berechne_Fib_Summe(n):
+def berechne_fib_summe(n):
     """Diese Funktion gibt die Summe der generierten Fibonacci-Zahlen bis zur n-ten Fibonacci-Zahl zurueck."""
     if n < 0:
         raise ValueError("Es gibt nur n-te Fibonacci-Zahlen >= 0! n darf nicht negativ sein!")
-    fib = closure_Fibonacci()
+    fib = closure_fibonacci()
     fib_list = []
     for i in range(n):
         fib_list.append(fib())
     return(sum(fib_list))
 
-def anonyme_Differentiation(f, a, h = 0.00001):
+def anonyme_differentiation(f, a, h = 0.00001):
     """Diese Funktion berechnet die lokale Ableitung einer Funktion f fuer die Stelle a."""
     diff = lambda h: (f(a + h) - f(a)) / h
     return diff(h)
--- Functional Programming/test_Functional_Programming.py
+++ Functional Programming/test_Functional_Programming.py
@@ -1,28 +1,31 @@
 import pytest
-from Functional_Programming import berechne_Fib_Summe
-from Functional_Programming import closure_Fibonacci
-from Functional_Programming import anonyme_Differentiation
+from Functional_Programming import berechne_fib_summe
+from Functional_Programming import closure_fibonacci
+from Functional_Programming import anonyme_differentiation
 
-def test_closure_Fibonacci():
-    f = closure_Fibonacci()
+def test_closure_fibonacci():
+    f = closure_fibonacci()
     assert f() == 0
     assert f() == 1
     assert f() == 1
     assert f() == 2
     assert f() == 3
 
-def test_berechne_Fib_Summe():
-    assert berechne_Fib_Summe(0) == 0
-    assert berechne_Fib_Summe(5) == 7
-    with pytest.raises(ValueError):
-        assert berechne_Fib_Summe(-1)
+def test_berechne_fib_summe_korrekt():
+    assert berechne_fib_summe(0) == 0
+    assert berechne_fib_summe(5) == 7
 
-def test_anonyme_Differentiation():
+def test_berechne_fib_summe_negative_zahl():
+    with pytest.raises(ValueError) as valueerror:
+        berechne_fib_summe(-1)
+    assert str(valueerror.value) == "Es gibt nur n-te Fibonacci-Zahlen >= 0! n darf nicht negativ sein!"
+
+def test_anonyme_differentiation():
     f1 = lambda x: x + 1
-    assert round(anonyme_Differentiation(f1, 2), 2) == 1
+    assert round(anonyme_differentiation(f1, 2), 2) == 1
 
     f2 = lambda x: x**2
-    assert round(anonyme_Differentiation(f2, 2), 2) == 4
+    assert round(anonyme_differentiation(f2, 2), 2) == 4
 
     f3 = lambda x: x**2 + x + 1
-    assert round(anonyme_Differentiation(f3, 2), 2) == 5
+    assert round(anonyme_differentiation(f3, 2), 2) == 5
